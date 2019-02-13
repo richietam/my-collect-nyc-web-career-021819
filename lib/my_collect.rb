8fc157f22110507874140ca7ac08a434b27f0096
richardtam.ny <richardtam.ny@gmail.com>
2019-02-13 21:34:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,8 @@
 def my_collect(empty_array)
   i = 0
+  collection = []
   while i < empty_array.length
-    yield(empty_array[i])
+    collection << yield(empty_array[i])
     i += 1
   end
   empty_array
